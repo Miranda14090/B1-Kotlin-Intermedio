91434dedbc372b8cb6efc33b684d5a68e0ef9edc
Daniel Coutiño <ing.dantonio@gmail.com>
2019-12-09 03:03:00 -0600

    Sesión 3 Ejemplo 1 uploaded

---
+++ Sesion-03/Ejemplo-01/EjemploActivity/app/src/main/java/org/bedu/ejemploactivity/ExampleActivity.kt
@@ -0,0 +1,14 @@
+package org.bedu.ejemploactivity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ExampleActivity: AppCompatActivity(){
+
+    override fun onCreate( savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_example)
+
+    }
+
+}
